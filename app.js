70bb238d460236c6675c9b9679e8064259d2978d
emanuel-gauna <lemagauna@gmail.com>
2023-01-17 19:34:30 -0300

    agregando los formularios y las views login.html y register.html

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { join } = require("path");
 const app = express();
 const path = require("path");
 const PORT = 3030;
@@ -9,6 +10,14 @@ app.use(express.static("views"));
 /* home */
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"))
+});
+/* register */
+app.post("/register", (req, res) =>{
+    res.sendFile(path,join(__dirname, "views/register.html"))
+});
+/* login */
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname,"/views/login.html"))
 });
 
 app.listen( PORT, () => console.log(`server listen in port ${PORT}\n http://localhost:${PORT}`));
