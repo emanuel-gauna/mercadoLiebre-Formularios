040fbd60fdff8c49663b417ef280bef432e6254c
emanuel-gauna <lemagauna@gmail.com>
2023-01-17 17:54:13 -0300

    inicializando el proyecto mercadoLiebre-formularios

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3030;
+
+app.use(express.static("public"));
+app.use(express.static("views"));
+/* home */
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,"/views/index.html"))
+});
+
+app.listen( PORT, () => console.log(`server listen in port ${PORT}\n http://localhost:${PORT}`));
